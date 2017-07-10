d9304916a42ec1fae48d80bd8061b1fd403bad81
sksat <sksat.tec@gmail.com>
2017-07-10 19:47:18 +0900

    [FIX] path

--- install_vim.sh
+++ install_vim.sh
@@ -1,5 +1,5 @@
 #!/usr/local/bin/bash
-git clone https://github.com/vim/vim.git $1
+git clone https://github.com/vim/vim.git $1/vim
 $1/vim/src/configure --prefix=$2 --enable-multibyte
 make -C $1/vim/src
 make -C $1/vim/src install
